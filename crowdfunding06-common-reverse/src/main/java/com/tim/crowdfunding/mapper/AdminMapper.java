e740dca0697b1f7d332d6c4fccf4922aa5a6272b
Tim <zhttim1805@gmail.com>
2021-12-20 23:49:53 +0800

    Create basic environment and generate entity and mappers

---
+++ crowdfunding06-common-reverse/src/main/java/com/tim/crowdfunding/mapper/AdminMapper.java
@@ -0,0 +1,31 @@
+package com.tim.crowdfunding.mapper;
+
+import com.tim.crowdfunding.entity.Admin;
+import com.tim.crowdfunding.entity.AdminExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface AdminMapper {
+    int countByExample(AdminExample example);
+
+    int deleteByExample(AdminExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Admin record);
+
+    int insertSelective(Admin record);
+
+    List<Admin> selectByExample(AdminExample example);
+
+    Admin selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Admin record, @Param("example") AdminExample example);
+
+    int updateByExample(@Param("record") Admin record, @Param("example") AdminExample example);
+
+    int updateByPrimaryKeySelective(Admin record);
+
+    int updateByPrimaryKey(Admin record);
+}
