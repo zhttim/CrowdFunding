f41acba39259810b28c866330fe796894df5aacd
Tim <zhttim1805@gmail.com>
2021-12-21 13:38:30 +0800

    spring整合mybatis

---
+++ crowdfunding01-admin-parent/crowdfunding02-admin-webui/src/test/java/com/tim/crowdfunding/test/CrowdTest.java
@@ -0,0 +1,39 @@
+package com.tim.crowdfunding.test;
+
+import com.tim.crowdfunding.entity.Admin;
+import com.tim.crowdfunding.mapper.AdminMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+//指定spring给junit提供的运行器类
+@RunWith(SpringJUnit4ClassRunner.class)
+//加载spring配置文件注解
+@ContextConfiguration(locations = {"classpath:spring-persist-mybatis.xml"})
+public class CrowdTest {
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private AdminMapper adminMapper;
+
+    @Test
+    public void testInsertAdmin(){
+        Admin admin = new Admin(null, "tom", "123456", "汤姆", "tom@qq.com", null);
+        int i = adminMapper.insert(admin);
+        System.out.println("受影响行数: i");
+    }
+
+    @Test
+    public void testConnection() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+    }
+
+}
