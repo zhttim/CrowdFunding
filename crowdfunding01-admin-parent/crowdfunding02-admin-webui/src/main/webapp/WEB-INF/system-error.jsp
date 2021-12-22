e870664d00482dfae9de35d20ca44486033a915d
Tim <zhttim1805@gmail.com>
2021-12-22 18:15:07 +0800

    配置异常解析器

---
+++ crowdfunding01-admin-parent/crowdfunding02-admin-webui/src/main/webapp/WEB-INF/system-error.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: tim
+  Date: 2021/12/22
+  Time: 4:34 下午
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>出错了</h1>
+${requestScope.exception.message}
+</body>
+</html>
---
+++ crowdfunding01-admin-parent/crowdfunding03-admin-component/src/main/java/com/tim/crowdfunding/mvc/config/CrowdExceptionResolver.java
@@ -0,0 +1,53 @@
+package com.tim.crowdfunding.mvc.config;
+
+import com.google.gson.Gson;
+import com.tim.crwodfunding.util.CrowdUtil;
+import com.tim.crwodfunding.util.ResultEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@ControllerAdvice表示基于注解的异常处理器类
+@ControllerAdvice
+public class CrowdExceptionResolver {
+
+    //@ExceptionHandler将异常与方法关联
+    // 空指针异常
+    @ExceptionHandler(NullPointerException.class)
+    public ModelAndView resolveNullPointerException(NullPointerException exception, HttpServletRequest request,
+                                                    HttpServletResponse response) throws IOException {
+        String viewName = "system-error";
+        return commonResolve(viewName, exception, request, response);
+    }
+
+    // 创建通用方法
+    private ModelAndView commonResolve(String viewName, Exception exception,
+                                       HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //判断当前请求类型
+        boolean judgeResult = CrowdUtil.judgeRequestType(request);
+        //如果为Ajax请求
+        if (judgeResult) {
+            //创建 ResultEntity 对象
+            ResultEntity<Object> resultEntity = ResultEntity.failed(exception.getMessage());
+            //创建Gson对象
+            Gson gson = new Gson();
+            //将ResultEntity对象转换为JSON字符串
+            String json = gson.toJson(resultEntity);
+            //将JSON字符作为响应体返回给浏览器
+            response.getWriter().write(json);
+            // 上面已经通过原生response对象返回了响应，因此不再提供ModelAndView对象
+            return null;
+        }
+        //如果不是Ajax请求，则创建ModelAndView对象
+        ModelAndView modelAndView = new ModelAndView();
+        //将Exception对象存入模型
+        modelAndView.addObject("exception", exception);
+        //设置对应的视图名称
+        modelAndView.setViewName(viewName);
+        return modelAndView;
+    }
+}
